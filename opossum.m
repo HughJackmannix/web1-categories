450c753138a0b23655806e711d2d3031fb1c4644
HughJackmannix <48026190+HughJackmannix@users.noreply.github.com>
2019-05-03 12:48:39 -0400

    Create opossum.m

---
+++ opossum.m
@@ -0,0 +1,12 @@
+---
+title: Opossum
+habitat: forrest
+size: small
+classification: mammal
+endangered: false
+predator: false 
+image: /img/bobcatprofilepic.png
+---
+
+The opossum (/əˈpɒsəm/ or /pɒsəm/) is a marsupial of the order Didelphimorphia (/daɪˌdɛlfɪˈmɔːrfiə/) endemic to the Americas. The largest order of marsupials in the Western Hemisphere, it comprises 103 or more species in 19 genera. Opossums originated in South America and entered North America in the Great American Interchange following the connection of the two continents. Their unspecialized biology, flexible diet, and reproductive habits make them successful colonizers and survivors in diverse locations and conditions.
+Although the animal is often called a possum in North America,[4] which would refer to the Virginia opossum species, it should not be confused with the suborder Phalangeriformes, which are arboreal marsupials in the Eastern Hemisphere also called "possums" because of their resemblance to Didelphimorphia.
